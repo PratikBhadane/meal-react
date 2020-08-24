0c9c291e397ec97a3def0963aacc65143f8151fd
Pratik Bhadane <pratikbhadane100@gmail.com>
2020-08-24 09:11:19 +0530

    Updates

---
+++ precache-manifest.7c57b4db0412f8f6cbbf975d3a13720a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5e40d46f1b36c0db7970da205b88e2",
+    "url": "/meal-react/index.html"
+  },
+  {
+    "revision": "9eea3f4c9179e0e3d312",
+    "url": "/meal-react/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "420d2e47136926bed35f",
+    "url": "/meal-react/static/css/main.8ec4cce0.chunk.css"
+  },
+  {
+    "revision": "9eea3f4c9179e0e3d312",
+    "url": "/meal-react/static/js/2.0f54705f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meal-react/static/js/2.0f54705f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420d2e47136926bed35f",
+    "url": "/meal-react/static/js/main.50c24640.chunk.js"
+  },
+  {
+    "revision": "d07e32f9c80748a76521",
+    "url": "/meal-react/static/js/runtime-main.5344aa73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meal-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
